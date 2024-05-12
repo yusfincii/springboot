4917236c05f3a6b5e9a83808c9b5b215824e3d45
yusuf <ysfnc27@gmail.com>
2024-05-13 00:37:25 +0300

    Actuator

--- 01-spring-boot-overview/03-actuator-demo/src/main/java/com/yusuf/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/03-actuator-demo/src/main/java/com/yusuf/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-    // expose "/" that return "Hello World"
 
+    // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
